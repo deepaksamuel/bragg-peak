6f09f53b912e88775d065e3dd5c2f5e7f9d3d9aa
deepaksamuel <deepaksamuel@gmail.com>
2020-08-05 15:32:47 +0530

    --

--- convert-table.py
+++ convert-table.py
@@ -1,5 +1,6 @@
 #%%
-# plots the depth-dose distribution from the datasets in data folder
+# merges all depth dose data into one single table
+# first column: energy; rest of the columns: dose at the depths
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,6 +9,8 @@ import os
 
 directory = "data" # the folder containing all the depth-dose data
 colnames=['X', 'Y', 'Z', 'Total Dose', 'Total2','Entries']
+colnames=['Energy','D1','D2']
+df_merged = pd.DataFrame()
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".txt"): #or filename.endswith(".py"): 
---
+++ erdf.py
@@ -0,0 +1,25 @@
+#%%
+# plots the ERDF distribution from the datasets in data folder
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+
+directory = "data" # the folder containing all the depth-dose data
+colnames=['X', 'Y', 'Z', 'Total Dose', 'Total2','Entries']
+for i in range(70,180) :
+    first_val=[]
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".txt"): #or filename.endswith(".py"): 
+            #print(os.path.join(directory, filename))
+            df = pd.read_csv(os.path.join(directory, filename),skiprows=3,header=0,names=colnames)   
+            first_val.append(df['Total Dose'][i])
+
+            continue
+        else:
+            continue
+    plt.plot(first_val)
+
+# %%
