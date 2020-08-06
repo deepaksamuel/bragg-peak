ed61706eb35256f9cbd6bef281439cba7dc032ec
deepaksamuel <deepaksamuel@gmail.com>
2020-08-06 22:02:19 +0530

    --

--- convert-table.py
+++ convert-table.py
@@ -9,17 +9,30 @@ import os
 
 directory = "data" # the folder containing all the depth-dose data
 colnames=['X', 'Y', 'Z', 'Total Dose', 'Total2','Entries']
-colnames=['Energy','D1','D2']
-df_merged = pd.DataFrame()
+colnames_merged=['Energy','Position','Dose']
+d =[]
+
+
+def getEnergy(fileName):
+    e =fileName.replace("-MEV-10000000-EVTS.txt","")
+    return float(e)
+
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".txt"): #or filename.endswith(".py"): 
         #print(os.path.join(directory, filename))
         df = pd.read_csv(os.path.join(directory, filename),skiprows=3,header=0,names=colnames)   
-        plt.plot(df['Z'],df['Total Dose'])
+        energy =getEnergy(filename)
+        for i in range(0,299):
+            d.append([energy,df['Z'][i],df['Total Dose'][i]])
+        #plt.plot(df['Z'],df['Total Dose'])
+        
         continue
     else:
         continue
+df_merged = pd.DataFrame(d,columns=colnames_merged)
+plt.plot(df_merged['Energy'],df_merged['Dose'])
+#
 
 
 # %%
